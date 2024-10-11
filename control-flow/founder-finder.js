156e5eb9302db0833c9ecfdbe365ffe6bbc698b5
sharmila657 <sharmilajirel8@gmail.com>
2024-10-11 21:37:56 +0545

    day 3

---
+++ control-flow/founder-finder.js
@@ -0,0 +1,13 @@
+let name = "Sharmi";
+ name = "Lucky" ;
+let found = true ;
+ if(name == "Sharmi"){
+    console.log(found);
+ }
+ else if(name == "Lucky")
+    {
+    console.log("false")
+    }
+ else{
+    console.log("Not Found");
+ }
