a82deffff645879c90b7047e4b0678601d2ddcd6
sharmila657 <sharmilajirel8@gmail.com>
2024-10-11 21:43:44 +0545

    day 2

---
+++ control-flow/must-be-perfect.js
@@ -0,0 +1,8 @@
+let word = "Perfect"
+if(word[0] == "P" && word.length == 7){
+    let checkedWord = word.toLocaleUpperCase()
+    console.log(checkedWord)
+}
+else{
+    console.log("The word is not starting with P")
+}
