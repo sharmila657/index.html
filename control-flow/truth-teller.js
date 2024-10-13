dcb1d442b1f7cc9da900c6f8b279c8d9c4bb349d
sharmila657 <sharmilajirel8@gmail.com>
2024-10-13 17:16:42 +0000

    day 4

---
+++ control-flow/truth-teller.js
@@ -0,0 +1,17 @@
+let boolean1 = true, boolean2 = false;
+
+if (boolean1 && boolean2){
+    result = "both"
+    console.log(result)
+
+}
+
+else if(boolean1 || boolean2){
+    result = "one"
+    console.log(result)
+}
+
+else {
+    result = "none"
+    console.log(result)
+}
