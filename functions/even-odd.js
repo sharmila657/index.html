79a0a15b08735ef2f3ee159c2c132ea4f51ed4ce
sharmila657 <sharmilajirel8@gmail.com>
2024-10-14 21:13:13 +0545

    day 5

---
+++ functions/even-odd.js
@@ -0,0 +1,9 @@
+function checkevenOdd(a){
+    if(a%2==0){                             
+        return "Even"
+    }else{
+        return "Odd"
+    }
+}
+let result = checkevenOdd(1)
+console.log(result)
