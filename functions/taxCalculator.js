72e9d4b8b70c6d41cb4b020df7542109f441d01c
sharmila657 <sharmilajirel8@gmail.com>
2024-10-18 22:02:55 +0545

    day 6 , functions

--- functions/taxCalculator.js
+++ functions/taxCalculator.js
@@ -2,9 +2,12 @@ function taxCalculator(price, state){
 if(state == "ktm"){
     return price * 0.15;
 }
+else if(state == "jiri"){
+    return price * 0.10;
+}
 else{
 return 0;
 }
 }
-
 console.log(taxCalculator(100, "ktm"))
+console.log(taxCalculator(100, "jiri"))
