9bc795d1ea94d01207a74df751fc8fa38ed26e2f
sharmila657 <sharmilajirel8@gmail.com>
2024-10-21 22:18:33 +0545

    strictly and losely

--- coercion-truthiness/howequel.js
+++ coercion-truthiness/howequel.js
@@ -1,7 +1,9 @@
 function howEqual(num1, num2){
-    if(num1 === num2){
+    if(num1 === num2){   
+        // it checks datatypes and value both
         return "strictly"
     }
+    // this checks only value not datatypes
     else if(num1 == num2){
         return "losely"
     }
