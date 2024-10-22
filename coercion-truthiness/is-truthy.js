ff6cbd12d491de0cf0494adaaed38550e19c2516
sharmila657 <sharmilajirel8@gmail.com>
2024-10-22 22:25:25 +0545

    day 8: truthy

---
+++ coercion-truthiness/is-truthy.js
@@ -0,0 +1,26 @@
+function isTruthy(name) {
+        if (name === "sharmi") {
+            return true;
+        }
+        else {
+            if (name === false) {
+                return 'The boolean value false is falsey';
+            }
+             else if (name === null) {
+                return 'The null value is falsey';
+            
+            }
+           else if (name === undefined) {
+                return 'undefined is falsey';
+            }
+           else if (name === 0) {
+                return 'The number 0 is falsey (the only falsey number)'
+                ;
+            }
+           else if (name === '') {
+                return 'The empty string is falsey (the only falsey string)';
+            }
+        }
+    
+    }
+    console.log(isTruthy());
