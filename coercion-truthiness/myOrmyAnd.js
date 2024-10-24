810a2e1c68b6d044a0c831cf5e2560536d020f98
sharmila657 <sharmilajirel8@gmail.com>
2024-10-24 21:17:09 +0545

    day 10 - myAnd

--- coercion-truthiness/myOrmyAnd.js
+++ coercion-truthiness/myOrmyAnd.js
@@ -14,17 +14,18 @@ else{
 
 function myAnd(c1,c2,c3){
     if(!c1){
-        return c3;                           
+        return c1;                           
     }
     else if(!c2){
-        return c3
+        return c2
     }
     else{
         return c3
     }
 }
 // console.log(myOr(true, true, false))
-console.log(myAnd(true,true,false))
+// console.log(myAnd(true,true,false))
+console.log(myAnd(1,0,1))
 
 
 
