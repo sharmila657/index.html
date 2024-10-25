a421f0ba2d04d51ba0cec01269b847a96cf98d63
sharmila657 <sharmilajirel8@gmail.com>
2024-10-25 22:31:16 +0545

    day 10 only one truthy without using ==

--- coercion-truthiness/onlyOne.js
+++ coercion-truthiness/onlyOne.js
@@ -1,9 +1,9 @@
 function onlyOne(arg1, arg2, arg3){
-if(arg1 || arg2 || arg3 == true){
+if(arg1 || arg2 || arg3){
     return true;
 }
 else{
     return false;
 }
 }
-console.log(onlyOne(false, false, false))
+console.log(onlyOne(true, false, false))
