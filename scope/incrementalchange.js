e221bc33e12c5d47c3b7cf29917d2a7feb3858b9
sharmila657 <sharmilajirel8@gmail.com>
2024-10-26 22:16:44 +0545

    day 12: scope it keeps increasing.

--- scope/incrementalchange.js
+++ scope/incrementalchange.js
@@ -4,8 +4,11 @@ function incrementalChange(num){
    counter += num;
     return counter;
 }
+console.log(incrementalChange(10))
 console.log(incrementalChange(1))
 console.log(incrementalChange(0))
 console.log(incrementalChange(-1))
+
+
 
 
