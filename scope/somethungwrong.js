df4706de60a96efd5b066e29416f9b3f2ffc1d1b
sharmila657 <sharmilajirel8@gmail.com>
2024-10-27 21:04:28 +0545

    day 13 somethingwrong

--- scope/somethungwrong.js
+++ scope/somethungwrong.js
@@ -3,8 +3,11 @@ function somethingwrong(num1, num2){
     let sum =0;
     sum += num1;
     sum += num2;
-
     return sum;
 
 }
-console.log(somethingwrong(1,2))
+// console.log(somethingwrong(1,2))
+console.log(somethingwrong(10,20))
+
+
+
